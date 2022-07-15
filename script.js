4b180590ae0ccabf662d00e6b86594c85b4dcd37
Taaaaab <owen.whitsett@gmail.com>
2022-07-15 15:56:24 -0400

    Add containers, grid change

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
 const container = document.querySelector('#container');
 const gameBox = document.querySelector('#gameBox');
-let gameBoard = [];
+
+const gameBoard = () => {
+    let myArray = [];
+
+        gameBox.textContent = myArray;
+};
+gameBoard();
+
+
+const Player = (name, team) => {
+    const getTeam = () => team;
+    const getName  = () => name;
+    const win = () => {
+        console.log('You Win!');
+    }
+    const score = score => {
+        score += 1;
+        if (score >= 3) {
+          win();
+        }
+      };
+    
+    const attack = enemy => {
+      if (getTeam < enemy.getTeam()) {
+        score(1);
+        console.log(`${enemy.getName()} has damaged ${name}`);
+      }
+      if (attack >= enemy.getTeam()) {
+        enemy.score(1);
+        console.log(`${name} has damaged ${enemy.getName()}`);
+      }
+    };
+    return {attack, score, getTeam, getName};
+  };
+  
+  const playerO = Player('jim', 10);
+  const playerX = Player('jeff', 5);
+  playerO.attack(playerX);
