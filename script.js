93b8b1c3f46b85c16bab078456f88365e910600b
Taaaaab <owen.whitsett@gmail.com>
2022-07-22 09:46:07 -0400

    Add reset button to gameboard

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const box6 = document.querySelector('#box6');
 const box7 = document.querySelector('#box7');
 const box8 = document.querySelector('#box8');
 const box9 = document.querySelector('#box9');
+const newGameBtn = document.querySelector('#newGameBtn');
 
 const gameBoard = () => {
   let boardArray = ['X','O','X','O','O','X','X','O','X'];
@@ -21,6 +22,26 @@ const gameBoard = () => {
       box7.textContent = `${boardArray[6]}`;
       box8.textContent = `${boardArray[7]}`;
       box9.textContent = `${boardArray[8]}`;
+
+  const gameBoardReset = (boardArray) => {
+    boardArray = ['','','','','','','','','',];
+    console.log(boardArray);
+    box1.textContent = `${boardArray[0]}`;
+    box2.textContent = `${boardArray[1]}`;
+    box3.textContent = `${boardArray[2]}`;
+    box4.textContent = `${boardArray[3]}`;
+    box5.textContent = `${boardArray[4]}`;
+    box6.textContent = `${boardArray[5]}`;
+    box7.textContent = `${boardArray[6]}`;
+    box8.textContent = `${boardArray[7]}`;
+    box9.textContent = `${boardArray[8]}`;
+  };
+  
+  newGameBtn.addEventListener('click', () => {
+    gameBoardReset(boardArray);
+  });
+  return {gameBoardReset};
+
 };
 gameBoard();
 
